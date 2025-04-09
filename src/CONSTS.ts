598bb599aa8f22867ab217bae138e3762fb7d102
Bernie Blume <bernie.blume@antsle.com>
2025-04-08 19:43:19 -0700

    Using 8900 for wss

--- src/CONSTS.ts
+++ src/CONSTS.ts
@@ -2,4 +2,4 @@ import { PublicKey } from "@solana/web3.js";
 
 export const CURRENT_ERA = 'Deep South'
 export const PROGRAM = new PublicKey("xSHLJPXU8QW3A9kGiRoL94bksJ7ZZPY4dUwJPAT8CVK");
-export const XANDEUM_WS = "wss://api.devnet.xandeum.com:8899";
+export const XANDEUM_WS = "wss://api.devnet.xandeum.com:8900";
