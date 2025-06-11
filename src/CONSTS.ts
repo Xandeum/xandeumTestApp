a1cf9401bb06341dde8484f435a1e16323429b45
Kaveen Laksitha <kaveen_lak@bitoku.io>
2025-06-11 17:42:37 +0530

    updated the ws to use wss

--- src/CONSTS.ts
+++ src/CONSTS.ts
@@ -2,5 +2,5 @@ import { PublicKey } from "@solana/web3.js";
 
 export const CURRENT_ERA = 'Deep South'
 export const PROGRAM = new PublicKey("xSHLJPXU8QW3A9kGiRoL94bksJ7ZZPY4dUwJPAT8CVK");
-export const XANDEUM_WS = "ws://8.52.151.4:8900";
+export const XANDEUM_WS = "wss://apis.trynet.xandeum.com";
 
